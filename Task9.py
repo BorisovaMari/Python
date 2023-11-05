2eb54761b395a2b793cf4479d942a9bd7f67ff46
BorisovaMari <143727644+BorisovaMari@users.noreply.github.com>
2023-11-05 16:05:48 +0300

    Add files via upload

---
+++ Task9.py
@@ -0,0 +1,29 @@
+import threading
+import time
+
+numbers = list(reversed(range(1, 11)))
+#print(numbers)
+
+class ClockThread(threading.Thread):
+    def __init__(self, num, interval, flow_lock):
+        super().__init__()
+        self.daemon = True
+        self.num = num
+        self.interval = interval
+        self.flow_lock = flow_lock
+
+    def run(self):
+        while True:
+            self.flow_lock.acquire(1)
+            print("Поток", self.num, ":", numbers, ". Поток", self.num, "завершил работу")
+            self.flow_lock.release()
+            time.sleep(self.interval)
+
+flow_lock = threading.Lock()
+
+t1 = ClockThread(1, 1, flow_lock)
+t2 = ClockThread(2, 1, flow_lock)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
